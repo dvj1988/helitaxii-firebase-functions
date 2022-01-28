5e4bcd2d648a6aa08e278051c4aaf885391d129d
Diljit VJ <diljitvj@gmail.com>
2022-01-29 02:31:54 +0530

    Update seed and gitignore

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@ import aircraftsApp from "./apps/aircrafts";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-const firebaseHttps = functions.region("asia-south1").https.onRequest;
+const firebaseHttps = functions
+  .runWith({
+    minInstances: 1,
+    memory: "256MB",
+  })
+  .region("asia-south1").https.onRequest;
 
 export const pilots = firebaseHttps(pilotsApp);
 export const aircrafts = firebaseHttps(aircraftsApp);
